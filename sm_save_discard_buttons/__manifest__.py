396623a3b2821183e894955027b9fd1a1080b709
Steven Marp <stevenmarp@users.noreply.github.com>
2026-06-13 16:48:15 +0700

    Update Odoo 19 version

--- sm_save_discard_buttons/__manifest__.py
+++ sm_save_discard_buttons/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Modern Save and Discard Buttons',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Extra Tools',
     'summary': 'Show clear Save and Discard buttons on Odoo form views',
     'description': """
